1822e04e5bad8ddcc0adfb921c4296bd32a1fb24
cristian.orozco <cristian.orozco@globant.com>
2021-04-12 16:46:33 -0500

    cropper and perspective image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useRef, useCallback } from 'react'
+import { Upload } from 'antd'
+import Cropper from 'react-perspective-cropper'
 
-function App() {
+const { Dragger } = Upload
+
+const App = () => {
+  const [cropState, setCropState] = useState()
+  const [imageCropped, setImageCropped] = useState()
+  const [image1, setImage1] = useState()
+  const [image2, setImage2] = useState()
+  const cropperRef = useRef()
+  const canvasRef = useRef()
+
+  console.log(image1)
+
+  const onDragStop = useCallback((s) => setCropState(s), [])
+  const onChange = useCallback((s) => setCropState(s), [])
+
+  const saveImage = async () => {
+    try {
+      console.log(cropperRef.current.done)
+      const res = await cropperRef.current.done({
+        preview: true,
+        filterCvParams: {
+          thMeanCorrection: 9,
+          thMode: window.cv.ADAPTIVE_THRESH_GAUSSIAN_C,
+        },
+      })
+      return res
+    } catch (e) {
+      console.log('error', e)
+    }
+  }
+
+  const takePictureAgain = () => cropperRef.current.backToCrop()
+  const onImgSelection = (e) => {
+    if (e.fileList && e.fileList.length > 0) {
+      const fileList = e.fileList[0].originFileObj
+      setImageCropped(fileList)
+    }
+  }
+
+  const draggerProps = {
+    name: 'file',
+    multiple: false,
+    onChange: onImgSelection,
+  }
+
+  const resetImage = () => {
+    setImageCropped(undefined)
+    setCropState()
+  }
+
+  const getImage = (setter) => {
+    const canvas = canvasRef.current.children[0].children[0]
+    const dataURI = canvas.toDataURL()
+    setter(dataURI)
+    resetImage()
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <h1>Cropper</h1>
+      <div>
+        <div className='container' ref={canvasRef}>
+          <Cropper
+            openCvPath='./opencv/opencv.js'
+            ref={cropperRef}
+            image={imageCropped}
+            onChange={onChange}
+            onDragStop={onDragStop}
+            maxWidth={window.innerWidth - 30}
+          />
+        </div>
+        {cropState?.loading && <h1>Loading...</h1>}
+        {!imageCropped && (
+          <Dragger {...draggerProps}>
+            <p>+</p>
+          </Dragger>
+        )}
+        {cropState && (
+          <>
+            <button onClick={saveImage}>Guardar</button>
+            <button onClick={takePictureAgain}>Editar</button>
+            <button onClick={resetImage}>Tomar foto de nuevo</button>
+            <button onClick={() => getImage(setImage1)}>Imagen</button>
+            <button onClick={() => getImage(setImage2)}>Imagen</button>
+          </>
+        )}
+        {image1 && <img src={image1} alt='imagen' />}
+        {image2 && <img src={image2} alt='imagen' />}
+      </div>
+    </>
+  )
 }
 
-export default App;
+export default App
